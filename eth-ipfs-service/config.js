db02e0c41135a15da7f925a478949db7ceec4ee0
apple <apple@MacBook-Pro-2.local>
2018-10-02 06:14:06 +0800

    eth-ipfs-service

---
+++ eth-ipfs-service/config.js
@@ -0,0 +1,12 @@
+const config = {
+    express: {
+        url: `http://localhost:`,  
+        port: '3005'     
+    },
+
+    db: {
+        url:`mongodb://localhost:27017/test`,
+    }
+}
+
+module.exports = config;
